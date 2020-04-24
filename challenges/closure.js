695cacf9e60fc03b0881e965e5f3a48cf843defe
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-04-24 11:46:13 -0500

    completed Task C: Prototypes

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,18 @@ myFunction();
 
 // Explanation: 
 
+// nestedFunction is an inner function to myFunction and is inside that 'block'.  Since internal is declared within that 'block', it is accessible to children inside. 
+
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function factorial(x){
+  let fac = 0; 
+  for(i=1; i<=x; i++) 
+    fac += i; 
+  return fac; 
+}
+
+console.log(factorial(4)); 
